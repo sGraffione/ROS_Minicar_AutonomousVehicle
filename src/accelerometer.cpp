5eec776b9955506802c737607aa07636824d3ceb
sGraffione <simo.graffio@gmail.com>
2023-05-22 11:55:52 +0200

    Changed accelerometer data acquisition

--- src/accelerometer.cpp
+++ src/accelerometer.cpp
@@ -9,7 +9,11 @@
 #include <cstdlib>
 #include <chrono>
 
-double Ts = 0.1;
+double Ts = 0.05;
+
+void readGyro(int, offset, int offsetAx, int offsetAy, int sensorPos1){
+	
+}
 
 int main(int argc, char **argv){
 	ros::init(argc, argv, "accelerometer");
@@ -20,18 +24,9 @@ int main(int argc, char **argv){
 	minicar::accel accelData;
 	
 	MPU6050 device(0x68);
-	float ax, ay, az, gr, gp, gy; //Variables to store the accel, gyro and angle values
+	float ax, ay, az, gr, gp, gy, omega; //Variables to store the accel, gyro and angle values
 	float yaw = 0;
 	sleep(1); //Wait for the MPU6050 to stabilize
-
-/*
-	//Calculate the offset
-	std::cout << "Calculating the offsets...\n    Please keep the accelerometer level and still\n    This could take a couple of minutes...";
-	device.getOffsets(&ax, &ay, &az, &gr, &gp, &gy);
-	std::cout << "Gyroscope R,P,Y: " << gr << "," << gp << "," << gy << "\nAccelerometer X,Y,Z: " << ax << "," << ay << "," << az << "\n";
-*/
-
-	//Read the current yaw angle
 	
 	
 	ros::Rate loop_rate(1/Ts);
@@ -40,58 +35,50 @@ int main(int argc, char **argv){
 	logFile.open("/home/pi/accel.dat", std::ios::out | std::ios::binary);
 	std::stringstream strDatFile(std::stringstream::out | std::stringstream::binary);
 */
-	if (n.hasParam("yaw")){
+	// Read Yaw initial condition
+	if (n.hasParam("/accelerometer/yaw")){
 		n.getParam("/accelerometer/yaw", yaw);
 	}else{
 		yaw = 0.0;
 	}
 	
 	/*Gyroscope drift calibration*/
-	std::vector<double> ayVect;
+	std::vector<double> gyVect, axVect, ayVect;
 	ROS_INFO("Calculating gyroscope offset...");
 	auto start = std::chrono::high_resolution_clock::now();
 	auto time = std::chrono::duration_cast<std::chrono::seconds>(start-start);
 	while(time.count() <= 5){ // 5 seconds of data
-		device.getGyro(&gr, &gp, &gy);
-		ayVect.push_back(gy);
-		//std::cout << time.count() << std::endl;
+		device.getGyroRaw(&gr, &gp, &gy);
+		device.getAccelRaw(&ax,&ay,&az);
+		gyVect.push_back(gy);
+		axVect.push_back(ax);
+		ayVect.push_back(ay);
+
 		auto stop = std::chrono::high_resolution_clock::now();
 		time = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
 	}
-	float offset = (std::accumulate(ayVect.begin(),ayVect.end(),0.0)/ayVect.size())*M_PI/180;
-	std::cout << offset << std::endl;
 	
-	//float offset = 0.5*M_PI/180;
+	float offset = (std::accumulate(gyVect.begin(),gyVect.end(),0.0)/gyVect.size())*M_PI/180;
+	float offsetAx = (std::accumulate(axVect.begin(),axVect.end(),0.0)/axVect.size())*M_PI/180;
+	float offsetAy = (std::accumulate(ayVect.begin(),ayVect.end(),0.0)/ayVect.size())*M_PI/180;
+	
 	while(ros::ok()){
 		
 		ros::spinOnce();
 		
-
-		//device.getAngle(0, &gr);
-		//device.getAngle(1, &gp);
-		//device.calc_yaw = true;
-		//device.getAngle(2, &gy);
+		double Ts = 0;
+		float omega = 0;
+		float accelx, accely;
 		
+		device.getGyroRaw(&gr, &gp, &gy);
+		device.getAccelRaw(&ar, &ap, &ay);
+		omega = (round((gy-offset)*10000 / GYRO_SENS)/10000)*M_PI/180;
+		accelx = (round((ax-offsetAx)*10000 / ACCEL_SENS)/10000)*9.81;
+		accely = (round((ay-offsetAy)*10000 / ACCEL_SENS)/10000)*9.81;
 		
-		//std::cout << "Current angle around the roll axis: " << gr << "\n";
-		//std::cout << "Current angle around the pitch axis: " << gp << "\n";
-		//std::cout << "Current angle around the yaw axis: " << yaw << "\n";
-		
-		//Get the current accelerometer values
-		//device.getAccel(&ax, &ay, &az);
-		//std::cout << "Accelerometer Readings: X: " << ax << ", Y: " << ay << ", Z: " << az << "\n";
-
-		
-		//logFile << gr << " " << gp << " " << gy << " " << ax << " " << ay << " " << az << " ";
-		//device.calc_yaw = false;
-		
-		//Get the current gyroscope values
-		device.getGyro(&gr, &gp, &gy);
-		//std::cout << "Gyroscope Readings: X: " << gr << ", Y: " << gp << ", Z: " << gy << "\n";
-		//logFile << gr << " " << gp << " " << gy << std::endl;
-		gy = gy*M_PI/180-offset;
-		yaw = yaw+Ts*(gy);
-		accelData.yaw = yaw;
+		accelData.omega = omega;
+		accelData.xAccel = accelx;
+		accelData.yAccel = accely;
 		current_pub.publish(accelData);
 		
 		loop_rate.sleep();
